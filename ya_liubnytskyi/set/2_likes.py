cf687d793a136fcf24b81ec782793d3e570e360d
Yaroslav <kindermalish@gmail.com>
2023-12-25 22:42:40 +0200

    hw(ya_liubnytskyi): set

--- ya_liubnytskyi/set/2_likes.py
+++ ya_liubnytskyi/set/2_likes.py
@@ -3,3 +3,15 @@
 # Функція приймає список користувачів (наприклад, 10), які відмітили пост лайком та список користувачів,
 # які прокоментували той же самий пост.
 # Функція повертає імена унікальних користувачів, які лайкнули і прокоментували пост одночасно.
+
+
+def user_activity(user_like, user_comment):
+    users_list = set(user_like) & set(user_comment)
+    return list(users_list)
+
+
+user_like = ["user1", "user2", "user3", "user4",]
+user_comment = ["user1","user3","user5","user6",]
+
+result = user_activity(user_like, user_comment)
+print(result)
--- ya_liubnytskyi/set/4_symbols.py
+++ ya_liubnytskyi/set/4_symbols.py
@@ -1,2 +1,12 @@
 # 4. Напишіть функцію, яка приймає рядок символів (наприклад, "a14b6fh")
 # і з використанням списків і множин визначає, чи всі символи в рядку унікальні.
+def unique_symbol(user_string):
+    if len(set(user_string)) == len(list(user_string)):
+        print("Всі символи в рядку унікальні.")
+    else:
+        print("Є символи які повторюються ")
+    
+
+user_string = "a14b6fh"
+unique_symbol(user_string)
+    

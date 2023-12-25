5c76de1c96627ff6a212ce37c09c34e9b1cb3b98
BanditosTM <banditostm123@gmail.com>
2023-12-25 18:48:21 +0300

    hw(ya_trufanov): set

--- ya_trufanov/set/2_likes.py
+++ ya_trufanov/set/2_likes.py
@@ -3,3 +3,18 @@
 # Функція приймає список користувачів (наприклад, 10), які відмітили пост лайком та список користувачів,
 # які прокоментували той же самий пост.
 # Функція повертає імена унікальних користувачів, які лайкнули і прокоментували пост одночасно.
+
+
+def get_activity_users(likes, comments):
+    likes = set(likes)
+    comments = set(comments)
+
+    intersection = likes & comments
+    return intersection
+
+
+user_likes = ["user1", "user2", "user3", "user4", "user5", "user6"]
+user_comments = ["user1", "user3", "user5", "user10"]
+
+result = get_activity_users(user_likes, user_comments)
+print(result)

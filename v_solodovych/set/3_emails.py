8ee3f4b5e6ecb2df89bccbee71b8799a747444e8
Volodymyr Solodovych <v.solodovych@ugi.edu.ua>
2023-12-27 21:41:35 +0200

    hw(v_solodovych): set

--- v_solodovych/set/3_emails.py
+++ v_solodovych/set/3_emails.py
@@ -1,3 +1,20 @@
 # 3. Напишіть функцію, яка приймає на вхід список електронних адрес
 # і повертає перелік унікальних поштових серверів з цього переліку
 # (множину всіх унікальних доменних імен, які зустрічаються в списку).
+
+def server_collector(emails):
+    server_set = set()
+    temp = None
+    counter = 0
+    
+    for _ in emails:
+        temp = emails[counter].split('@')
+        server_set.add(temp[1])
+        counter += 1
+
+    return server_set
+
+emails_list = ['qwert2000@ukr.net', 'lazylow@gmail.com', 'sethome@i.ua', 'micha2003@gmail.com']
+server_list = list(server_collector(emails_list))
+
+print(server_list)
--- v_solodovych/set/4_symbols.py
+++ v_solodovych/set/4_symbols.py
@@ -1,2 +1,14 @@
 # 4. Напишіть функцію, яка приймає рядок символів (наприклад, "a14b6fh")
 # і з використанням списків і множин визначає, чи всі символи в рядку унікальні.
+def check_uniquity(symbols):
+    symbols_list = list(symbols)
+    symbols_set = set(symbols)
+
+    if len(symbols_list) != len(symbols_set):
+        return "Символи в рядку не є унікальними."
+    else:
+        return "Символи в рядку є унікальними."
+
+symbols = input("Введіть рядок символів: ")
+
+print(check_uniquity(symbols))
